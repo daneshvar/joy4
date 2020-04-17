8c3ae7c291a184ea9d6d024f78b6177c4f0ff9be
sepehr <sepehr.r201400@gmail.com>
2020-04-17 16:02:52 +0430

    remove unused code

--- format/format.go
+++ format/format.go
@@ -2,21 +2,13 @@ package format
 
 import (
 	"github.com/daneshvar/joy4/av/avutil"
-	"github.com/daneshvar/joy4/format/aac"
-	"github.com/daneshvar/joy4/format/flv"
 	"github.com/daneshvar/joy4/format/mp4"
 	"github.com/daneshvar/joy4/format/raw"
-	"github.com/daneshvar/joy4/format/rtmp"
 	"github.com/daneshvar/joy4/format/rtsp"
-	"github.com/daneshvar/joy4/format/ts"
 )
 
 func RegisterAll() {
 	avutil.DefaultHandlers.Add(mp4.Handler)
-	avutil.DefaultHandlers.Add(ts.Handler)
-	avutil.DefaultHandlers.Add(rtmp.Handler)
 	avutil.DefaultHandlers.Add(rtsp.Handler)
-	avutil.DefaultHandlers.Add(flv.Handler)
-	avutil.DefaultHandlers.Add(aac.Handler)
 	avutil.DefaultHandlers.Add(raw.Handler)
 }
